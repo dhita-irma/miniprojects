9dec5b8beaac8a0a55ee9a1b1cf4fa3090ebea79
Dhita Irma <dhita.irma@gmail.com>
2020-03-18 15:24:29 +0800

    Create exercise3.py
    Find specific item in a linked list

---
+++ data-structures/linkedlist-exercises/exercise3.py
@@ -0,0 +1,50 @@
+# Write a Python program to search a specific item in a singly linked list and
+# return true if the item is found otherwise return false
+
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.count = 0
+
+    def append(self, value):
+        new_item = Node(value)
+        if not self.head:
+            self.head = new_item
+            self.tail = new_item
+        else:
+            self.tail.next = new_item
+            self.tail = new_item
+        self.count += 1
+
+    def iterate(self):
+        current = self.head
+        while current:
+            print(current.value)
+            current = current.next
+        print(f"Linked list size = {str(self.count)}")
+
+    def find(self, value):
+        current = self.head
+        while current.value != value and current.next:
+            current = current.next
+        print(current.value == value)
+
+
+if __name__ == '__main__':
+
+    list0 = LinkedList()
+    list0.append('Python')
+    list0.append('Java')
+    list0.append('JavaScript')
+    list0.append('C')
+
+    list0.find('Java')
+    list0.find('C++')
