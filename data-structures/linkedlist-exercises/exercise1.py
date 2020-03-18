84dfc3f9b4bd9374a877a38bebaaea108a070ebe
Dhita Irma <dhita.irma@gmail.com>
2020-03-18 14:54:38 +0800

    Create exercise1.py
    Create singly linked list
    Append some items
    Iterate through the lsit

---
+++ data-structures/linkedlist-exercises/exercise1.py
@@ -0,0 +1,29 @@
+#  Write a Python program to create a singly linked list,
+#  append some items and iterate through the list.
+
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def append(self, value):
+        new_item = Node(value)
+        if not self.head:
+            self.head = new_item
+            self.tail = new_item
+        else:
+            self.tail.next = new_item
+            self.tail = new_item
+
+    def iterate(self):
+        current = self.head
+        while current:
+            print(current.value)
+            current = current.next
