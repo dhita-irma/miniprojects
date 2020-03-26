6ec41fadbeb3a7b037a3481ab2b7d650082812e4
Dhita Irma <dhita.irma@gmail.com>
2020-03-26 21:15:21 +0800

    Update queue.py
    Remove self.count

--- datastructure/queue.py
+++ datastructure/queue.py
@@ -5,29 +5,26 @@ class Queue:
 
     def __init__(self):
         self.queue = DoublyLinkedList()
-        self.count = 0
 
     def enqueue(self, item):
         """Add an item to the end of the queue"""
         self.queue.append(item)
-        self.count += 1
 
     def dequeue(self):
         """Remove and return the value of an item at the front of the queue"""
-        if self.count > 0:
+        if self.queue.count > 0:
             value = self.peek()
             self.queue.remove_at_index(0)
-            self.count -= 1
             return value
         else:
             return None
 
     def peek(self):
         """Return value of the item at the front of the list without removing it"""
-        if self.count > 0:
+        if self.queue.count > 0:
             return self.queue.head.value
         else:
             return None
 
     def get_size(self):
-        return self.count
+        return self.queue.count
