f6d44892d3b63023143d1678a1b7f9c3afcedb2d
Dhita Irma <dhita.irma@gmail.com>
2020-04-02 10:51:15 +0800

    Update recursion_exercise.py

--- recursion/recursion_exercise.py
+++ recursion/recursion_exercise.py
@@ -6,3 +6,12 @@
 # Calculate how many steps it takes to get to 1
 
 
+def collatz(n):
+    if n == 1:
+        return 0
+    elif n % 2 == 0:
+        n = n/2
+    else:
+        n = (3*n)+1
+    return collatz(n) + 1
+
