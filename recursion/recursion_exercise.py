e111acfcd1e8508fc4c9345adf7f1833b2e37071
Dhita Irma <dhita.irma@gmail.com>
2020-04-02 10:35:12 +0800

    Create recursion_exercise.py
    Write instruction

---
+++ recursion/recursion_exercise.py
@@ -0,0 +1,8 @@
+# Collatz conjecture
+# If n is 1, stop.
+# if n is even repeat this process on n/2
+# if n is odd, repeat this process on 3n+1
+
+# Calculate how many steps it takes to get to 1
+
+
