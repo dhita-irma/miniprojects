b02bf099eacacd302c4e0947e0d3a729a44c84e0
Dhita Irma <dhita.irma@gmail.com>
2020-04-06 19:14:25 +0800

    Update trie.py
    Reimplement Trie using Node class

--- datastructure/trie.py
+++ datastructure/trie.py
@@ -1,25 +1,32 @@
+class Node:
+    def __init__(self, char):
+        self.char = char
+        self.children = {}
+        self.is_end = False
+
+
 class Trie:
 
     def __init__(self):
-        self.head = {}
+        self.root = Node('*')
 
     def insert(self, word):
-        current = self.head
+        current = self.root
 
         for char in word:
-            if char not in current:
-                current[char] = {}
-            current = current[char]
-        current['*'] = True
+            if char not in current.children:
+                current.children[char] = Node(char)
+            current = current.children[char]
+        current.is_end = True
 
     def find(self, word):
-        current = self.head
+        current = self.root
 
         for char in word:
-            if char not in current:
+            if char not in current.children:
                 return False
-            current = current[char]
-        if '*' in current:
+            current = current.children[char]
+        if current.is_end:
             return True
         else:
             return False
