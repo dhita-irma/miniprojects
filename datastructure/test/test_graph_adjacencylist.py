7f9d8fabd974bd49b045a3115d8d8a762b83af7f
Dhita Irma <dhita.irma@gmail.com>
2020-04-25 05:41:20 +0800

    Update test_graph_adjacencylist.py

--- datastructure/test/test_graph_adjacencylist.py
+++ datastructure/test/test_graph_adjacencylist.py
@@ -6,5 +6,5 @@ class TestGraphlist(unittest.TestCase):
 
     def test_add_empty(self):
         my_graph = Graph()
-        result = my_graph.add_vertex(Vertex("A"))
+        result = my_graph.add_vertex(Vertex("San Francisco"))
         self.assertTrue(result)
