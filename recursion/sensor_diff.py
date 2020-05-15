1b9d2a2fea21019683053c1b38440e8782c1877a
Dhita Irma <dhita.irma@gmail.com>
2020-05-15 11:49:40 +0800

    Update sensor_diff.py
    Reimplement sensor diff using recursion

--- recursion/sensor_diff.py
+++ recursion/sensor_diff.py
@@ -1,12 +1,14 @@
 def total_diff(sensorA, sensorB):
-    diff = 0
-    for i in range(len(sensorA)):
-        diff += abs(sensorA[i] - sensorB[i])
-    return diff
+    if len(sensorA) == 0:
+        return 0
+    else:
+        last_diff = abs(sensorA[-1] - sensorB[-1])
+        return total_diff(sensorA[:-1], sensorB[:-1]) + last_diff
 
 
 if __name__ == '__main__':
     sensorA = [15, -4, 56, 10, -23]
     sensorB = [14, -9, 56, 14, -23]
+
     diff = total_diff(sensorA, sensorB)
     print(f"The total difference is: {str(diff)}")
