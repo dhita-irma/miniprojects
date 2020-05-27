326055f786d9fa9f11883a81a9168eaa60c65235
Dhita Irma <dhita.irma@gmail.com>
2020-05-27 14:51:38 +0800

    Revert "Update turtle_maze.py"
    This reverts commit 35c3e55cefb71d0e1cc1ae6dccac7f3e7cbb0866.

--- recursion/turtle_maze.py
+++ recursion/turtle_maze.py
@@ -1,143 +1,30 @@
-import turtle
-
-PART_OF_PATH = 'O'
-TRIED = '.'
-OBSTACLE = '+'
-DEAD_END = '-'
 
 
-class Maze:
-    def __init__(self, mazeFileName):
-        rowsInMaze = 0
-        columnsInMaze = 0
-        self.mazelist = []
-        mazeFile = open(mazeFileName,'r')
-
-        # Read text file line by line,
-        # Convert it to a 2D array
-        for line in mazeFile:
-            rowList = []
-            col = 0
-            for ch in line[:-1]:
-                rowList.append(ch)
-                if ch == 'S':
-                    # Initate the coordinate of starting point
-                    self.startRow = rowsInMaze
-                    self.startCol = col
-                col = col + 1
-            rowsInMaze = rowsInMaze + 1
-            self.mazelist.append(rowList)
-            columnsInMaze = len(rowList)
-
-        self.rowsInMaze = rowsInMaze
-        self.columnsInMaze = columnsInMaze
-
-        # x and y Translate to determine the center of the maze
-        # This helps to draw the maze in the center of the Turtle Screen
-        self.xTranslate = -columnsInMaze/2
-        self.yTranslate = rowsInMaze/2
-
-        self.t = turtle.Turtle()
-        self.t.shape('turtle')
-        self.wn = turtle.Screen()
-
-        self.wn.setworldcoordinates(-(columnsInMaze - 1) / 2 - .5,   # x-coordinate of lower left corner of canvas
-                                    -(rowsInMaze - 1) / 2 - .5,      # y-coordinate of lower left corner of canvas
-                                    (columnsInMaze - 1) / 2 + .5,    # x-coordinate of upper right corner of canvas
-                                    (rowsInMaze - 1) / 2 + .5)       # y-coordinate of upper right corner of canvas
-
-    def drawMaze(self):
-        self.t.speed(10)
-        self.wn.tracer(0)
-        for y in range(self.rowsInMaze):
-            for x in range(self.columnsInMaze):
-                if self.mazelist[y][x] == OBSTACLE:
-                    self.drawCenteredBox(x + self.xTranslate, -y + self.yTranslate, 'orange')
-        self.t.color('black')
-        self.t.fillcolor('blue')
-        self.wn.update()
-        self.wn.tracer(1)
-
-    def drawCenteredBox(self, x, y, color):
-        self.t.up()
-        self.t.goto(x - .5, y - .5)
-        self.t.color(color)
-        self.t.fillcolor(color)
-        self.t.setheading(90)
-        self.t.down()
-        self.t.begin_fill()
-        for i in range(4):
-            self.t.forward(1)
-            self.t.right(90)
-        self.t.end_fill()
-
-    def moveTurtle(self, x, y):
-        self.t.up()
-        self.t.setheading(self.t.towards(x + self.xTranslate, -y + self.yTranslate))
-        self.t.goto(x + self.xTranslate, -y + self.yTranslate)
-
-    def dropBreadcrumb(self, color):
-        self.t.dot(10, color)
-
-    def updatePosition(self, row, col, val=None):
-        if val:
-            self.mazelist[row][col] = val
-        self.moveTurtle(col, row)
-
-        if val == PART_OF_PATH:
-            color = 'green'
-        elif val == OBSTACLE:
-            color = 'red'
-        elif val == TRIED:
-            color = 'black'
-        elif val == DEAD_END:
-            color = 'red'
-        else:
-            color = None
-
-        if color:
-            self.dropBreadcrumb(color)
-
-    def isExit(self, row, col):
-        return (row == 0 or
-                row == self.rowsInMaze - 1 or
-                col == 0 or
-                col == self.columnsInMaze - 1)
-
-    def __getitem__(self, idx):
-        return self.mazelist[idx]
-
-
-def searchFrom(maze, startRow, startColumn):
-    # try each of four directions from this point until we find a way out.
-    # base Case return values:
-    #  1. We have run into an obstacle, return false
-    maze.updatePosition(startRow, startColumn)
-    if maze[startRow][startColumn] == OBSTACLE:
+def search_from(maze, start_row, start_col):
+    maze.update_position(start_row, start_col)
+    # Check for base case
+    # 1. We run into a obstacle, return false
+    if maze[start_row, start_col] == OBSTACLE:
         return False
-    #  2. We have found a square that has already been explored
-    if maze[startRow][startColumn] == TRIED or maze[startRow][startColumn] == DEAD_END:
+    # 2. We have found a square that has already been explored
+    if maze[start_row, start_col] == TRIED:
         return False
-    # 3. We have found an outside edge not occupied by an obstacle
-    if maze.isExit(startRow, startColumn):
-        maze.updatePosition(startRow, startColumn, PART_OF_PATH)
+    # 3. success, an outside edge not occupied by an obstacle
+    if maze.is_exit(start_row, start_col):
+        maze.update_position(start_row, start_col, PART_OF_PATH)
         return True
-    maze.updatePosition(startRow, startColumn, TRIED)
-    # Otherwise, use logical short circuiting to try each direction
-    # in turn (if needed)
-    found = searchFrom(maze, startRow - 1, startColumn) or \
-            searchFrom(maze, startRow + 1, startColumn) or \
-            searchFrom(maze, startRow, startColumn - 1) or \
-            searchFrom(maze, startRow, startColumn + 1)
+    maze.update_position(start_row, start_col, TRIED)
+
+    # Otherwise, use logical short circuiting to try each
+    # direction in turn (if needed)
+
+    found = search_from(maze, start_row-1, start_col) or \
+        search_from(maze, start_row+1, start_col) or \
+        search_from(maze, start_row, start_col-1) or \
+        search_from(maze, start_row, start_col+1)
+
     if found:
-        maze.updatePosition(startRow, startColumn, PART_OF_PATH)
+        maze.update_position(start_row, start_col, PART_OF_PATH)
     else:
-        maze.updatePosition(startRow, startColumn, DEAD_END)
+        maze.update_position(start_row, start_col, DEAD_END)
     return found
-
-
-myMaze = Maze('maze0.txt')
-myMaze.drawMaze()
-myMaze.updatePosition(myMaze.startRow, myMaze.startCol)
-
-searchFrom(myMaze, myMaze.startRow, myMaze.startCol)
