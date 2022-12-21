b105664e368278446e7b2d9f63aab8551c26b222
dhita-irma <52138969+dhita-irma@users.noreply.github.com>
2022-12-21 12:28:16 -0800

    Simplify solution
    Refactor some variables to make it shorter and omit the sum variable and for loop to combine them in a single line return statement to make the solution more succinct

--- daily-coding/amstrong-numbers/amstrong_numbers.py
+++ daily-coding/amstrong-numbers/amstrong_numbers.py
@@ -1,11 +1,8 @@
 # Armstrong numbers are equal to the sum of the powers of their digits
 # Exponent is the length of the number, base 10
 
-def is_armstrong_number(number):
+def is_armstrong_number(number: int) -> bool:
     """Is number the equal to sum of powers of digits?"""
-    number_str = str(number)
-    digits = len(number_str)
-    sum = 0
-    for n in range(digits): 
-        sum += int(number_str[n]) ** digits
-    return sum == number
+    s = str(number)
+    exp = len(s)
+    return number == sum(int(digit) ** exp for digit in s)
