b016b21a2c3fc0b98568ffb7fd65f2a5e66f436a
dhita-irma <52138969+dhita-irma@users.noreply.github.com>
2022-12-21 17:42:02 -0800

    Create function two_fer

--- exercism/python/two-fer/two_fer.py
+++ exercism/python/two-fer/two_fer.py
@@ -1,2 +1,7 @@
-def two_fer(name):
-    pass
+def two_fer(name="you"):
+    """
+    Given a name variable, return a string with the message:
+    "One for [name], one for me". 
+    If [name] is missing, return "One for you, one for me."
+    """
+    return f"One for {name}, one for me."
